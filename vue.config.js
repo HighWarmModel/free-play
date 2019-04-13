d6e59b6c27da5478156d21fa7369e2c724e2077c
fire <1170533231@qq.com>
2019-04-13 23:02:59 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,13 @@ module.exports = {
     types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
     // 开启比gzip体验更好的Zopfli压缩
   },
+  // 修改webpack的配置
+  configureWebpack: {
+    // 把原本需要写在webpack.config.js中的配置代码 写在这里 会自动合并
+    externals: {
+      'wx': 'wx'
+    }
+  },
   // pluginOptions: {
   //   'style-resources-loader': {
   //     preProcessor: 'stylus',
